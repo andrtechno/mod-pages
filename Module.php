283735e85d4e45c8a033e0e477da740f745d7739
andrtechno <andrew.panix@gmail.com>
2017-08-15 15:53:55 +0300

    init

---
+++ Module.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\system\modules\pages;
+
+use Yii;
+use panix\engine\WebModule;
+
+class Module extends WebModule {
+
+
+    //public $routes = [
+    //    'page/<url>' => 'pages/default/view',
+    //];
+
+    public function getNav() {
+        return [
+            [
+                'label' => 'Станицы',
+                "url" => ['/admin/pages'],
+                'icon' => 'fa-edit'
+            ],
+            [
+                'label' => 'Настройки',
+                "url" => ['/admin/pages/settings'],
+                'icon' => 'fa-gear'
+            ]
+        ];
+    }
+
+    public function getInfo() {
+        return [
+            'name' => Yii::t('pages/default', 'MODULE_NAME'),
+            'author' => 'andrew.panix@gmail.com',
+            'version' => '1.0',
+            'icon' => 'icon-edit',
+            'description' => Yii::t('pages/default', 'MODULE_DESC'),
+            'url' => ['/admin/pages'],
+        ];
+    }
+
+    protected function getDefaultModelClasses() {
+        return [
+            //  'Pages' => 'panix\shop\models\Pages',
+            'Pages' => 'app\system\modules\pages\models\Pages',
+            'PagesSearch' => 'app\system\modules\pages\models\PagesSearch',
+        ];
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+mod-pages
+===========
+Module for CORNER CMS
+
+[![Latest Stable Version](https://poser.pugx.org/panix/mod-pages/v/stable)](https://packagist.org/packages/panix/mod-pages) [![Total Downloads](https://poser.pugx.org/panix/mod-pages/downloads)](https://packagist.org/packages/panix/mod-pages) [![Monthly Downloads](https://poser.pugx.org/panix/mod-pages/d/monthly)](https://packagist.org/packages/panix/mod-pages) [![Daily Downloads](https://poser.pugx.org/panix/mod-pages/d/daily)](https://packagist.org/packages/panix/mod-pages) [![Latest Unstable Version](https://poser.pugx.org/panix/mod-pages/v/unstable)](https://packagist.org/packages/panix/mod-pages) [![License](https://poser.pugx.org/panix/mod-pages/license)](https://packagist.org/packages/panix/mod-pages)
+
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist panix/mod-pages "*"
+```
+
+or add
+
+```
+"panix/mod-pages": "*"
+```
+
+to the require section of your `composer.json` file.
+
