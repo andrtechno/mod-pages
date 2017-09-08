828324f75c2f9b29de9e285132f176640f57e031
andrtechno <andrew.panix@gmail.com>
2017-09-08 16:29:01 +0300

    add migrate

--- migrations/m170908_125100_init.php
+++ migrations/m170908_125100_init.php
@@ -14,9 +14,9 @@ class m170908_125100_init extends Migration {
             'id' => $this->primaryKey(),
             'seo_alias' => $this->string(255)->notNull(),
             'user_id' => $this->integer(),
-            'date_create' => $this->datetime(),
-            'date_update' => $this->datetime(),
-            'ordern ' => $this->integer(),
+            'date_create' => $this->timestamp()->defaultValue(null),
+            'date_update' => $this->timestamp(),
+            'ordern' => $this->integer(),
             'switch' => $this->boolean()->defaultValue(1),
                 ], $tableOptions);
 
@@ -26,7 +26,7 @@ class m170908_125100_init extends Migration {
             'object_id' => $this->integer(),
             'language_id' => $this->string(2),
             'name' => $this->string(255),
-            'text' => $this->string(255),
+            'text' => $this->text(),
                 ], $tableOptions);
 
 
@@ -37,9 +37,29 @@ class m170908_125100_init extends Migration {
 
         $this->createIndex('object_id', '{{%pages_translate}}', 'object_id', 0);
         $this->createIndex('language_id', '{{%pages_translate}}', 'language_id', 0);
+
+        if ($this->db->driverName != "sqlite") {
+            $this->addForeignKey('{{%fk_pages_translate}}', '{{%pages_translate}}', 'object_id', '{{%pages}}', 'id', "CASCADE", "NO ACTION");
+        }
+
+        $columns = ['seo_alias', 'user_id', 'ordern', 'date_create'];
+        $this->batchInsert('{{%pages}}', $columns, [
+            ['about', 1, 1, date('Y-m-d H:i:s')],
+            ['mypage', 1, 2, date('Y-m-d H:i:s')],
+        ]);
+
+
+        $columns = ['object_id', 'language_id', 'name', 'text'];
+        $this->batchInsert('{{%pages_translate}}', $columns, [
+            [1, Yii::$app->language, 'О компании', ''],
+            [2, Yii::$app->language, 'Тест', ''],
+        ]);
     }
 
     public function down() {
+        if ($this->db->driverName != "sqlite") {
+            $this->dropForeignKey('{{%fk_pages_translate}}', '{{%pages_translate}}');
+        }
         $this->dropTable('{{%pages}}');
         $this->dropTable('{{%pages_translate}}');
     }
