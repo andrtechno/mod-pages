0e73eca2f07a2e63bba314bde9b0b1a2274ed63a
andrtechno <andrew.panix@gmail.com>
2017-09-19 15:07:49 +0300

    init

--- views/default/view.php
+++ views/default/view.php
@@ -1,2 +1,10 @@
+<?php
+use panix\engine\widgets\Pjax;
+Pjax::begin([
+    'timeout' => 5000,
+
+]);
+?>
 <h1><?= $model->isString('name'); ?></h1>
 <p><?= $model->renderText(); ?></p>
+<?php Pjax::end(); ?>
