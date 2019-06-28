33782981d180699f34e7e5d17fe196a4a7a83409
Andrew <andrew.panix@gmail.com>
2019-06-28 14:51:32 +0300

    init

--- models/Pages.php
+++ models/Pages.php
@@ -12,7 +12,8 @@ use panix\engine\db\ActiveRecord;
  * @property integer $id
  * @property string $name
  */
-class Pages extends ActiveRecord {
+class Pages extends ActiveRecord
+{
 
     const route = '/admin/pages/default';
     const MODULE_ID = 'pages';
@@ -21,11 +22,13 @@ class Pages extends ActiveRecord {
     //public $disallow_update = [32];
     //public $disallow_switch = [32];
 
-    public static function find() {
+    public static function find()
+    {
         return new PagesQuery(get_called_class());
     }
 
-    public function getGridColumns() {
+    public function getGridColumns()
+    {
         return [
             [
                 'attribute' => 'id',
@@ -53,7 +56,7 @@ class Pages extends ActiveRecord {
                     'options' => ['class' => 'form-control']
                 ]),
                 'contentOptions' => ['class' => 'text-center'],
-                'value' => function($model) {
+                'value' => function ($model) {
                     return Yii::$app->formatter->asDatetime($model->date_create, 'php:d D Y H:i:s');
                 }
             ],
@@ -67,7 +70,7 @@ class Pages extends ActiveRecord {
                     'options' => ['class' => 'form-control']
                 ]),
                 'contentOptions' => ['class' => 'text-center'],
-                'value' => function($model) {
+                'value' => function ($model) {
                     return Yii::$app->formatter->asDatetime($model->date_update, 'php:d D Y H:i:s');
                 }
             ],
@@ -83,64 +86,71 @@ class Pages extends ActiveRecord {
     /**
      * @inheritdoc
      */
-    public static function tableName() {
+    public static function tableName()
+    {
         return '{{%pages}}';
     }
 
     /**
      * @inheritdoc
      */
-    public function rules() {
+    public function rules()
+    {
         return [
             [['name', 'text', 'slug'], 'required'],
             [['name', 'slug'], 'string', 'max' => 255],
             [['name', 'slug'], 'trim'],
-            ['slug', '\panix\engine\validators\UrlValidator','attributeCompare'=>'name'],
+            ['slug', '\panix\engine\validators\UrlValidator', 'attributeCompare' => 'name'],
             ['slug', 'match',
                 'pattern' => '/^([a-z0-9-])+$/i',
-                'message' => Yii::t('app','PATTERN_URL')
+                'message' => Yii::t('app', 'PATTERN_URL')
             ],
             [['date_update', 'date_create'], 'safe'],
-                //[['date_update'], 'date', 'format' => 'php:U']
-                /// [['date_update'], 'date','format'=>'php:U', 'timestampAttribute' => 'date_update','skipOnEmpty'=>  true],
+            //[['date_update'], 'date', 'format' => 'php:U']
+            /// [['date_update'], 'date','format'=>'php:U', 'timestampAttribute' => 'date_update','skipOnEmpty'=>  true],
 //[['date_update','date_create'], 'filter','filter'=>'strtotime'],
         ];
     }
 
-    public function renderText() {
+    public function renderText()
+    {
         if (Yii::$app->user->can('admin')) {
             \panix\ext\tinymce\TinyMceInline::widget();
         }
         return (Yii::$app->user->can('admin')) ? $this->isText('text') : $this->pageBreak('text');
     }
 
-    public function getUser() {
+    public function getUser()
+    {
         return $this->hasOne(\panix\mod\user\models\User::class, ['id' => 'user_id']);
     }
 
-    public function getTranslations() {
+    public function getTranslations()
+    {
         return $this->hasMany($this->translationClass, ['object_id' => 'id']);
     }
+
     public function getTranslation()
     {
         return $this->hasOne($this->translationClass, ['object_id' => 'id']);
     }
 
 
-    public function behaviors() {
+    public function behaviors()
+    {
         return \yii\helpers\ArrayHelper::merge([
-                    'translate' => [
-                        'class' => TranslateBehavior::class,
-                        'translationAttributes' => [
-                            'name',
-                            'text'
-                        ]
-                    ],
-                    'commentBehavior' => [
-                        'class' => \panix\mod\comments\components\CommentBehavior::class,
-                        'owner_title' => 'name',
-                    ],
-                        ], parent::behaviors());
+            'translate' => [
+                'class' => TranslateBehavior::class,
+                'translationAttributes' => [
+                    'name',
+                    'text'
+                ]
+            ],
+            'commentBehavior' => [
+                'class' => \panix\mod\comments\components\CommentBehavior::class,
+                'owner_title' => 'name',
+            ],
+        ], parent::behaviors());
     }
 
 }
--- views/admin/default/update.php
+++ views/admin/default/update.php
@@ -2,31 +2,24 @@
 use yii\helpers\Html;
 use panix\engine\bootstrap\ActiveForm;
 use panix\ext\tinymce\TinyMce;
-?>
 
-<div class="card bg-light">
+$form = ActiveForm::begin();
+?>
+<div class="card">
     <div class="card-header">
         <h5><?= Html::encode($this->context->pageName) ?></h5>
     </div>
     <div class="card-body">
-        <?php
-        $form = ActiveForm::begin([
-                    'options' => ['class' => 'form-horizontal'],
-        ]);
-        ?>
-
         <?= $form->field($model, 'name')->textInput(['maxlength' => 255]) ?>
         <?= $form->field($model, 'slug')->textInput(['maxlength' => 255]) ?>
-
         <?=
         $form->field($model, 'text')->widget(TinyMce::class, [
             'options' => ['rows' => 6],
         ]);
         ?>
-        <div class="form-group text-center">
-            <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'CREATE') : Yii::t('app', 'UPDATE'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        </div>
-        <?php ActiveForm::end(); ?>
+    </div>
+    <div class="card-footer text-center">
+        <?= $model->submitButton(); ?>
     </div>
 </div>
-
+<?php ActiveForm::end(); ?>
