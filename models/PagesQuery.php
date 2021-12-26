0f2f89d6df875cfcd85c8d2bf97f8ae8b46b7b45
Andrew <andrew.panix@gmail.com>
2021-12-26 12:29:15 +0200

    Update PagesQuery.php

--- models/PagesQuery.php
+++ models/PagesQuery.php
@@ -18,4 +18,21 @@ class PagesQuery extends ActiveQuery {
         $this->addOrderBy(["{$tableName}.ordern" => SORT_DESC]);
         parent::init();
     }
+
+    public function header()
+    {
+        /** @var \yii\db\ActiveRecord $modelClass */
+        $modelClass = $this->modelClass;
+        $tableName = $modelClass::tableName();
+        return $this->andWhere(["{$tableName}.show_header" => 1]);
+    }
+
+    public function footer()
+    {
+        /** @var \yii\db\ActiveRecord $modelClass */
+        $modelClass = $this->modelClass;
+        $tableName = $modelClass::tableName();
+        return $this->andWhere(["{$tableName}.show_footer" => 1]);
+    }
+    
 }
